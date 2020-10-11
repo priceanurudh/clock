b10bc7a329bffb682f852058c10c383b23d5b59f
priceanurudh <72705306+priceanurudh@users.noreply.github.com>
2020-10-11 23:14:59 +0530

    Add files via upload

---
+++ homepage.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:realistic_clock/clock_view.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var now = DateTime.now();
+    var formattedTime = DateFormat('hh:mm').format(now);
+    var formattedDate = DateFormat('EEE, d MMM').format(now);
+    var timeZoneString = now.timeZoneOffset.toString().split('.').first;
+    var offsetSign = '';
+    if(!timeZoneString.startsWith('-')) offsetSign = '+';
+    print(timeZoneString);
+
+    return Scaffold(
+      backgroundColor: Color(0xFF2D2F41),
+      body: Row(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {  },
+                child: Column(
+                  children: <Widget>[
+                    FlutterLogo(),
+                    Text('Clock',
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          VerticalDivider(
+            color: Colors.white54,
+            width: 1,
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 32, vertical: 64),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Clock',
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  SizedBox(height: 32),
+                  Text(
+                    formattedTime,
+                    style: TextStyle(color: Colors.white, fontSize: 64),
+                  ),
+                  Text(
+                    formattedDate,
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  ClockView(),
+                  Text(
+                    'Timezone',
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.language,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 16),
+                      Text(
+                        'UTC' + offsetSign + timeZoneString,
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
